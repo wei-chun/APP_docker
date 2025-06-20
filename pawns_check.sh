94d368927e4d4fe226996b88365b596d30374da8
wei-chun <weichun.company@gmail.com>
2025-06-20 23:44:31 +0800

    Update pawns_check.sh
    Update checking string.

--- pawns_check.sh
+++ pawns_check.sh
@@ -3,7 +3,7 @@
 _log=$(docker logs --tail 1 naughty_poincare)
 echo "["`date "+%F %T"`"]" $_log | tee -a pawns_check.log
 
-if [[ $_log == *"could_not_mark_peer_alive"* ]]; then
+if [[ $_log == *"not_running"* ]]; then
   echo "["`date "+%F %T"`"]" "Pawns is disconnected. Try to reconnect it." | tee -a pawns_check.log
   _connectCMD=$(synowebapi --exec api=SYNO.Docker.Container version=1 method=stop name="naughty_poincare" && sleep 5 && docker container start naughty_poincare)
   echo "["`date "+%F %T"`"]" $_connectCMD | tee -a pawns_check.log
