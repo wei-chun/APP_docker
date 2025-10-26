ade14ca29a9cc5542536ea9ed602b968b4dbf784
wei-chun <weichun.company@gmail.com>
2025-10-26 19:06:02 +0800

    Update pawns_check.sh
    Corrected disconnection judgment conditions

--- pawns_check.sh
+++ pawns_check.sh
@@ -3,14 +3,14 @@
 _log=$(docker logs --tail 1 naughty_poincare)
 echo "["`date "+%F %T"`"]" $_log | tee -a pawns_check.log
 
-if [[ $_log == *"not_running"* ]]; then
+if [[ $_log == *"not_running"*  ||  $_log == *"starting"* ]]; then
   echo "["`date "+%F %T"`"]" "Pawns is disconnected. Try to reconnect it." | tee -a pawns_check.log
   _connectCMD=$(synowebapi --exec api=SYNO.Docker.Container version=1 method=stop name="naughty_poincare" && sleep 5 && docker container start naughty_poincare)
   echo "["`date "+%F %T"`"]" $_connectCMD | tee -a pawns_check.log
   sleep 60
   _log2=$(docker logs --tail 2 naughty_poincare)
   echo "["`date "+%F %T"`"]" $_log2 | tee -a pawns_check.log
-  while [[ $_log2 != *"running"* ]]
+  while [[ $_log2 == *"not_running"*  ||  $_log2 == *"starting"* ]];
   do
     echo "["`date "+%F %T"`"]" "Pawns connect failure. Try to reconnect it." | tee -a pawns_check.log
     _reconnectCMD=$(synowebapi --exec api=SYNO.Docker.Container version=1 method=stop name="naughty_poincare" && sleep 5 && docker container start naughty_poincare)
